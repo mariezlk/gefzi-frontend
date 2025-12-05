70e5dabea0bcc30301d671def98c623a2765b1d7
Marie Zielke <marie.zielke@telekom.de>
2025-12-05 13:48:44 +0100

    blocked days & days in past

--- src/components/CalendarElement.js
+++ src/components/CalendarElement.js
@@ -1,8 +1,10 @@
 import { Box, Flex, Text, Tooltip } from '@mantine/core';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import CheckOutlinedIcon from '@mui/icons-material/CheckOutlined';
 
 function CalendarElement({index, day, events, currentMonth, currentYear, holiday, calendar}) {
 
+    const today = new Date().toISOString().split('T')[0];
     const weekendIndex = [5, 6, 12, 13, 19, 20, 26, 27, 33, 34];
     const feiertage = holiday?.feiertage?.map((f) => f.date)
     const currentDate = `${currentYear}-${String(currentMonth + 1).padStart(2, '0')}-${String(day.day).padStart(2, '0')}`;
@@ -75,29 +77,50 @@ function CalendarElement({index, day, events, currentMonth, currentYear, holiday
 
     return (
         <> 
-            <Flex h="calc(60.5vh / 5)" direction='column' style={{ backgroundColor: day.isCurrentMonth ? "#fff" : "#F5F5F5", border: "2px solid rgb(0,198,178)", borderRadius: "5px", position: "relative" }}>
-                <Box style={{ position: "absolute", top: 0, left: 4 }}>
+            <Flex h="calc(60.5vh / 5)" direction='column' style={{ backgroundColor: day.isCurrentMonth ? "#fff" : "#F5F5F5", border: currentDate == today ? "3px solid rgb(249, 203, 0)" : "2px solid rgb(0,198,178)", borderRadius: "5px", position: "relative" }}>
+                <Box c={currentDate == today ? "rgb(249, 203, 0)" : "black"} style={{ position: "absolute", top: 0, left: 4, cursor: "default"}}>
                     {day.day}
                 </Box>
-                {(weekendIndex.includes(index) || feiertage?.includes(currentDate)) &&
+                {(weekendIndex.includes(index) || feiertage?.includes(currentDate)) && day.isCurrentMonth &&  currentDate >= today &&
                     <Tooltip c="black" bg="#F5F5F5" fz={14} px={7} offset={-65} label={`Wochenende/ Feiertag`}>
                         <Box align="center">
                             <Flex  justify="center" align="center" style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}>
                                 <CloseOutlinedIcon style={{color: "#F5F5F5", fontSize: 100, padding: 0}}/>
                             </Flex>
                             <Box style={{ position: "absolute", bottom: 0, left: 0 }}>
-                                {day.isCurrentMonth && <Text fz={23}>🌻</Text>}
+                                {day.isCurrentMonth && <Text style={{cursor: "default"}} fz={23}>🌻</Text>}
                             </Box>
                         </Box>
                     </Tooltip>
                 }
-                {!weekendIndex.includes(index) && !feiertage?.includes(currentDate) && day.isCurrentMonth && eventsOnDay &&
+                {currentDate < today && day.isCurrentMonth && 
+                    <Tooltip c="black" bg="#F5F5F5" fz={14} px={7} offset={-60} label={`Tag liegt in der Vergangenheit`}>
+                        <Box align="center">
+                            <Flex  justify="center" align="center" style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}>
+                                <CheckOutlinedIcon style={{color: "#F5F5F5", fontSize: 100, padding: 0}}/>
+                            </Flex>
+                        </Box>
+                    </Tooltip>
+                }
+                {!weekendIndex.includes(index) && !feiertage?.includes(currentDate) && day.isCurrentMonth && eventsOnDay && currentDate >= today &&
                     <Flex>
+                        {freeTimes.length == 0 && eventsOnDay.filter(e => e.visibillity === "business").length == 0 &&
+                            <Tooltip c="black" bg="#F5F5F5" fz={14} px={7} offset={{ mainAxis: -65, crossAxis: 95 }} label={`geblockter Tag/ keine freien Timeslots`}>
+                                <Box align="center">
+                                    <Flex  justify="center" align="center" style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}>
+                                        <CloseOutlinedIcon style={{color: "#F5F5F5", fontSize: 100, padding: 0}}/>
+                                    </Flex>
+                                    <Box style={{ position: "absolute", bottom: 0, left: 0 }}>
+                                        {day.isCurrentMonth && <Text fz={23} style={{cursor: "default"}}>⛔</Text>}
+                                    </Box>
+                                </Box>
+                            </Tooltip>
+                        }
                         {eventsOnDay
                             .filter(e => e.visibillity === "business")
                             .map((event, i) => (
                                 <Tooltip c="black" bg="#F5F5F5" fz={14} px={7} offset={30} label={`Teamtermin von ${event.startTime} bis ${event.endTime}`}>
-                                    <Box style={{position: "absolute", left: `${offsetForEvent(event) + 6}%`, bottom: 2, fontSize: 20}}>
+                                    <Box style={{cursor: "default", position: "absolute", left: `${offsetForEvent(event) + 6}%`, bottom: 2, fontSize: 20}}>
                                         🔥
                                     </Box>
                                 </Tooltip>
