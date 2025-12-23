a120fb9fcadf6f92ae196074040f1f7143129936
Marie Zielke <marie.zielke@telekom.de>
2025-12-23 13:35:19 +0100

    login redirect p2

--- src/components/EntryRoute.js
+++ src/components/EntryRoute.js
@@ -2,10 +2,6 @@ import { Navigate } from "react-router-dom";
 
 function EntryRoute({ user }) {
 
-    if (user === undefined) {
-        return null;
-    }
-
     if (!user) {
         return <Navigate to="/login" replace />;
     }
